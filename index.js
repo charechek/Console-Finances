3e5dc3e89d39f68f4142e6924a06f6818905f741
unknown <cezary.bakula@gmail.com>
2022-12-14 21:02:15 +0000

    added script lines in script.js to calculate how many months are included into datasheet and that info is logged into console

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var finances = [
+let finances = [
 ['Jan-2010', 867884],
 ['Feb-2010', 984655],
 ['Mar-2010', 322013],
@@ -86,3 +86,6 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+let total = finances.length;
+console.log("Total number of months in datasheet is: " + total);
